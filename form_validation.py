95bcbd886960d84f41ead171abd9f4b1d10ec020
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-02-24 23:08:17 +0100

    Naming, user_exist function

--- form_validation.py
+++ form_validation.py
@@ -4,7 +4,7 @@ import re
 ############################################################
 
 
-def is_valid_name_string(name_string):
+def is_valid_name(name_string):
     if not name_string:
         return False
     if not isinstance(name_string, str):
@@ -15,7 +15,7 @@ def is_valid_name_string(name_string):
     return True
 
 
-def is_valid_email_address_string(email_string):
+def is_valid_email_address(email_string):
     if not email_string:
         return False
     if not isinstance(email_string, str):
@@ -26,7 +26,7 @@ def is_valid_email_address_string(email_string):
     return True
 
 
-def is_valid_password_string(password_string):
+def is_valid_password(password_string):
     if not password_string:
         return False
     if not isinstance(password_string, str):
@@ -47,3 +47,14 @@ def is_user_value_unique(property_name, value):
             return False
 
     return True
+
+
+def user_exist(property_name, value):
+    if not value or not property_name:
+        return False
+
+    for user in USERS:
+        if user[property_name] == value:
+            return True
+
+    return False
