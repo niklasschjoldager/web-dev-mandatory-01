885563226dcb51ac6652ab69ed60165d718f6264
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-02-24 23:09:03 +0100

    Reformatting dummy user + tweet, JWT secret

--- g.py
+++ g.py
@@ -1,13 +1,24 @@
 USERS = [
     {
         "id": "5f5f311d-ca9b-481b-8846-f7db449270f8",
-        "username": "niklas-schjoldager",
+        "username": "niklasschjoldager",
         "first_name": "Niklas",
         "last_name": "Schjoldager",
-        "email": "nikl1565@stud.kea.dk",
-        "password": "mycoolpassword123",
+        "email": "test@mail.dk",
+        "password": "1234",
     }
 ]
+SESSIONS = []
+TWEETS = [
+    {
+        "id": "1f62ebd4-3b6a-4b06-9ff9-24542fca3095",
+        "created_at": 1645722067,
+        "message": "This is my first tweet. Hello tweeter!",
+        "user_id": "5f5f311d-ca9b-481b-8846-f7db449270f8",
+    }
+]
+
+JSON_WEB_TOKEN_SECRET = "My super token secret"
 
 REGEX_EMAIL = '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
