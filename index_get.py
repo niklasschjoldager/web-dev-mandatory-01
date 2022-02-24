48ff927ae791f3c3f19278283837cba566059a42
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-02-24 23:09:32 +0100

    If user logged in, redirect from / to /home

--- index_get.py
+++ index_get.py
@@ -1,7 +1,20 @@
-from bottle import get, view
+from bottle import get, view, redirect, request
+import jwt
+from g import JSON_WEB_TOKEN_SECRET, SESSIONS
 
 ############################################################
 @get("/")
 @view("index")
 def _():
+    encoded_user_session = request.get_cookie("user_session")
+
+    if not encoded_user_session:
+        return
+
+    decoded_user_session = jwt.decode(encoded_user_session, JSON_WEB_TOKEN_SECRET, algorithms=["HS256"])
+    user_session = decoded_user_session["session_id"]
+
+    if user_session in SESSIONS:
+        return redirect("/home")
+
     return
