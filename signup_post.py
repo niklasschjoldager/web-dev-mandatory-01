6420446b79fe88465cfe7b5b4184410030dbb09a
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-02-24 23:10:25 +0100

    Refactored function naming

--- signup_post.py
+++ signup_post.py
@@ -1,22 +1,15 @@
 from bottle import post, redirect, request, response
 from g import USERS
+from user_auth import create_user_session
 from form_validation import (
     is_user_value_unique,
-    is_valid_email_address_string,
-    is_valid_name_string,
-    is_valid_password_string,
+    is_valid_email_address,
+    is_valid_name,
+    is_valid_password,
+    user_exist,
 )
 import uuid
 
-
-User = {
-    "id",
-    "username",
-    "first_name",
-    "last_name",
-    "email",
-    "password",
-}
 
 ############################################################
 @post("/signup")
@@ -27,10 +20,8 @@ def _():
     user_first_name = request.forms.get("user-first-name")
     user_last_name = request.forms.get("user-last-name")
 
-    print(USERS)
-
     # Validation
-    if not is_valid_name_string(user_username):
+    if not is_valid_name(user_username):
         response.status = 400
         return "Please enter a valid username"
 
@@ -38,23 +29,27 @@ def _():
         response.status = 400
         return "Your chosen username is taken, please try another one"
 
-    if not is_valid_email_address_string(user_email):
+    if not is_valid_email_address(user_email):
         response.status = 400
         return "Please enter an emailaddress"
 
-    if is_user_value_unique("email", user_email):
+    if user_exist("email", user_email):
         response.status = 400
         return "Email already exists. Forgot your password?"
 
-    if not is_valid_password_string(user_password):
+    if user_exist("username", user_username):
+        response.status = 400
+        return "Username already exists. Forgot your password?"
+
+    if not is_valid_password(user_password):
         response.status = 400
         return "Please enter a valid password"
 
-    if not is_valid_name_string(user_first_name):
+    if not is_valid_name(user_first_name):
         response.status = 400
         return "Please enter your first name"
 
-    if not is_valid_name_string(user_last_name):
+    if not is_valid_name(user_last_name):
         response.status = 400
         return "Please enter your last name"
 
@@ -67,5 +62,6 @@ def _():
         "password": user_password,
     }
     USERS.append(user)
+    create_user_session(user)
 
-    return f"{USERS}"
+    return redirect("/home")
